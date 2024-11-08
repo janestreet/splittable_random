971b1468c7d69bca56c8d24a55608ba6a62642b3
public-release <as-public-release@janestreet.com>
2024-11-08 22:02:20 +0000

    v0.18~preview.130.04+450

--- test/splittable_random_test.ml
+++ test/splittable_random_test.ml
@@ -36,217 +36,212 @@ let%expect_test "bool fairness" =
     |}]
 ;;
 
-let%test_module "int64" =
-  (module struct
-    open Int64.O
+module%test [@name "int64"] _ = struct
+  open Int64.O
 
-    let bounds =
-      [ Int64.min_value
-      ; Int64.min_value + 1L
-      ; Int64.min_value + 2L
-      ; -1_000_000_000L
-      ; -1_000_000L
-      ; -1_000L
-      ; -100L
-      ; -10L
-      ; -2L
-      ; -1L
-      ; 0L
-      ; 1L
-      ; 2L
-      ; 10L
-      ; 100L
-      ; 1_000L
-      ; 1_000_000L
-      ; 1_000_000_000L
-      ; Int64.max_value - 2L
-      ; Int64.max_value - 1L
-      ; Int64.max_value
-      ]
-    ;;
+  let bounds =
+    [ Int64.min_value
+    ; Int64.min_value + 1L
+    ; Int64.min_value + 2L
+    ; -1_000_000_000L
+    ; -1_000_000L
+    ; -1_000L
+    ; -100L
+    ; -10L
+    ; -2L
+    ; -1L
+    ; 0L
+    ; 1L
+    ; 2L
+    ; 10L
+    ; 100L
+    ; 1_000L
+    ; 1_000_000L
+    ; 1_000_000_000L
+    ; Int64.max_value - 2L
+    ; Int64.max_value - 1L
+    ; Int64.max_value
+    ]
+  ;;
 
-    let%test_unit "bounds" =
-      let open Int64.O in
-      let state = Splittable_random.of_int 0 in
-      List.iter bounds ~f:(fun lo ->
-        List.iter bounds ~f:(fun hi ->
-          if lo <= hi
+  let%test_unit "bounds" =
+    let open Int64.O in
+    let state = Splittable_random.of_int 0 in
+    List.iter bounds ~f:(fun lo ->
+      List.iter bounds ~f:(fun hi ->
+        if lo <= hi
+        then
+          for _ = 1 to 1_000 do
+            let choice = Splittable_random.int64 state ~lo ~hi in
+            if choice < lo || choice > hi
+            then
+              Error.raise_s
+                [%message "out of bounds" (choice : int64) (lo : int64) (hi : int64)]
+          done))
+  ;;
+
+  let%test_unit "coverage" =
+    let state = Splittable_random.of_int 0 in
+    List.iter [ 1L; 10L; 100L; 1000L ] ~f:(fun range ->
+      let lo = 0L in
+      let hi = Int64.pred range in
+      for _ = 1 to 100 do
+        let count = Array.init (Int64.to_int_exn range) ~f:(fun _ -> 0) in
+        for _ = 1 to Int64.to_int_exn (range * 100L) do
+          let i = Splittable_random.int64 state ~lo ~hi |> Int64.to_int_exn in
+          count.(i) <- Int.succ count.(i)
+        done;
+        Array.iteri count ~f:(fun value count ->
+          if Int.equal count 0
           then
-            for _ = 1 to 1_000 do
-              let choice = Splittable_random.int64 state ~lo ~hi in
-              if choice < lo || choice > hi
-              then
-                Error.raise_s
-                  [%message "out of bounds" (choice : int64) (lo : int64) (hi : int64)]
-            done))
-    ;;
+            Error.raise_s
+              [%message
+                "failed to generate value" (value : int) (lo : int64) (hi : int64)])
+      done)
+  ;;
 
-    let%test_unit "coverage" =
-      let state = Splittable_random.of_int 0 in
-      List.iter [ 1L; 10L; 100L; 1000L ] ~f:(fun range ->
-        let lo = 0L in
-        let hi = Int64.pred range in
-        for _ = 1 to 100 do
-          let count = Array.init (Int64.to_int_exn range) ~f:(fun _ -> 0) in
-          for _ = 1 to Int64.to_int_exn (range * 100L) do
-            let i = Splittable_random.int64 state ~lo ~hi |> Int64.to_int_exn in
-            count.(i) <- Int.succ count.(i)
-          done;
-          Array.iteri count ~f:(fun value count ->
-            if Int.equal count 0
+  (* This should return values with mean 0 and variance 1 if implementation
+       is correct. *)
+  let test_bias_of_mean ~lo ~hi ~sample_size state =
+    let open Int64.O in
+    assert (lo < hi);
+    let lof = Int64.to_float lo in
+    let hif = Int64.to_float hi in
+    let delta = hif -. lof in
+    let draw () =
+      (Int64.to_float (Splittable_random.int64 state ~lo ~hi) -. lof) /. delta
+    in
+    let rec loop iters accum =
+      if iters <= 0L then accum else loop (iters - 1L) (accum +. draw ())
+    in
+    let sum = loop sample_size 0. in
+    let mean = sum /. Int64.to_float sample_size in
+    let n = delta +. 1. in
+    (* We have n evenly spaced values from 0 to 1 inclusive, each with probability 1/n.
+         This has variance (n+1)/(12(n-1)) per draw.  *)
+    let standard_error =
+      Float.sqrt ((n +. 1.) /. (12. *. (n -. 1.) *. Int64.to_float sample_size))
+    in
+    (mean -. 0.5) /. standard_error
+  ;;
+
+  let%test_unit "bias" =
+    let open Float.O in
+    let hi = 3689348814741910528L in
+    (* about 0.4 * max_int *)
+    let z =
+      test_bias_of_mean ~lo:0L ~hi ~sample_size:1000L (Splittable_random.of_int 0)
+    in
+    assert (Stdlib.abs_float z < 3.)
+  ;;
+end
+
+module%test [@name "float"] _ = struct
+  let bounds =
+    [ Float.neg Float.max_finite_value
+    ; -1_000_000_000.
+    ; -1.
+    ; -0.000_000_001
+    ; 0.
+    ; 0.000_000_001
+    ; 1.
+    ; 1_000_000_000.
+    ; Float.max_finite_value
+    ]
+  ;;
+
+  let%test_unit "bounds" =
+    let open Float.O in
+    let state = Splittable_random.of_int 0 in
+    List.iter bounds ~f:(fun lo ->
+      List.iter bounds ~f:(fun hi ->
+        if lo < hi
+        then
+          for _ = 1 to 1000 do
+            let float = Splittable_random.float state ~lo ~hi in
+            if float < lo || float > hi
+            then
+              Error.raise_s
+                [%message "float out of bounds" (float : float) (lo : float) (hi : float)]
+          done))
+  ;;
+
+  let%test_unit "coverage" =
+    let open Float.O in
+    let state = Splittable_random.of_int 0 in
+    List.iter bounds ~f:(fun lo ->
+      List.iter bounds ~f:(fun hi ->
+        if lo < hi
+        then
+          for _ = 1 to 100 do
+            let hi' = (lo *. 0.01) +. (hi *. 0.99) in
+            let lo' = (lo *. 0.99) +. (hi *. 0.01) in
+            let mid1 = (lo *. 0.51) +. (hi *. 0.49) in
+            let mid2 = (lo *. 0.49) +. (hi *. 0.51) in
+            let saw_hi = ref false in
+            let saw_lo = ref false in
+            let saw_mid = ref false in
+            for _ = 1 to 1000 do
+              let float = Splittable_random.float state ~lo ~hi in
+              if float < lo' then saw_lo := true;
+              if float > hi' then saw_hi := true;
+              if float > mid1 && float < mid2 then saw_mid := true
+            done;
+            if not (!saw_lo && !saw_mid && !saw_hi)
             then
               Error.raise_s
                 [%message
-                  "failed to generate value" (value : int) (lo : int64) (hi : int64)])
-        done)
-    ;;
+                  "did not get coverage of lo, mid, and hi values"
+                    (lo : float)
+                    (hi : float)
+                    (!saw_lo : bool)
+                    (!saw_hi : bool)
+                    (!saw_mid : bool)]
+          done))
+  ;;
 
-    (* This should return values with mean 0 and variance 1 if implementation
-       is correct. *)
-    let test_bias_of_mean ~lo ~hi ~sample_size state =
-      let open Int64.O in
-      assert (lo < hi);
-      let lof = Int64.to_float lo in
-      let hif = Int64.to_float hi in
-      let delta = hif -. lof in
-      let draw () =
-        (Int64.to_float (Splittable_random.int64 state ~lo ~hi) -. lof) /. delta
-      in
-      let rec loop iters accum =
-        if iters <= 0L then accum else loop (iters - 1L) (accum +. draw ())
-      in
-      let sum = loop sample_size 0. in
-      let mean = sum /. Int64.to_float sample_size in
-      let n = delta +. 1. in
-      (* We have n evenly spaced values from 0 to 1 inclusive, each with probability 1/n.
-         This has variance (n+1)/(12(n-1)) per draw.  *)
-      let standard_error =
-        Float.sqrt ((n +. 1.) /. (12. *. (n -. 1.) *. Int64.to_float sample_size))
-      in
-      (mean -. 0.5) /. standard_error
-    ;;
-
-    let%test_unit "bias" =
-      let open Float.O in
-      let hi = 3689348814741910528L in
-      (* about 0.4 * max_int *)
-      let z =
-        test_bias_of_mean ~lo:0L ~hi ~sample_size:1000L (Splittable_random.of_int 0)
-      in
-      assert (Stdlib.abs_float z < 3.)
-    ;;
-  end)
-;;
-
-let%test_module "float" =
-  (module struct
-    let bounds =
-      [ Float.neg Float.max_finite_value
-      ; -1_000_000_000.
-      ; -1.
-      ; -0.000_000_001
-      ; 0.
-      ; 0.000_000_001
-      ; 1.
-      ; 1_000_000_000.
-      ; Float.max_finite_value
-      ]
-    ;;
-
-    let%test_unit "bounds" =
-      let open Float.O in
-      let state = Splittable_random.of_int 0 in
-      List.iter bounds ~f:(fun lo ->
-        List.iter bounds ~f:(fun hi ->
-          if lo < hi
-          then
-            for _ = 1 to 1000 do
-              let float = Splittable_random.float state ~lo ~hi in
-              if float < lo || float > hi
-              then
-                Error.raise_s
-                  [%message
-                    "float out of bounds" (float : float) (lo : float) (hi : float)]
-            done))
-    ;;
-
-    let%test_unit "coverage" =
-      let open Float.O in
-      let state = Splittable_random.of_int 0 in
-      List.iter bounds ~f:(fun lo ->
-        List.iter bounds ~f:(fun hi ->
-          if lo < hi
-          then
-            for _ = 1 to 100 do
-              let hi' = (lo *. 0.01) +. (hi *. 0.99) in
-              let lo' = (lo *. 0.99) +. (hi *. 0.01) in
-              let mid1 = (lo *. 0.51) +. (hi *. 0.49) in
-              let mid2 = (lo *. 0.49) +. (hi *. 0.51) in
-              let saw_hi = ref false in
-              let saw_lo = ref false in
-              let saw_mid = ref false in
-              for _ = 1 to 1000 do
-                let float = Splittable_random.float state ~lo ~hi in
-                if float < lo' then saw_lo := true;
-                if float > hi' then saw_hi := true;
-                if float > mid1 && float < mid2 then saw_mid := true
-              done;
-              if not (!saw_lo && !saw_mid && !saw_hi)
-              then
-                Error.raise_s
-                  [%message
-                    "did not get coverage of lo, mid, and hi values"
-                      (lo : float)
-                      (hi : float)
-                      (!saw_lo : bool)
-                      (!saw_hi : bool)
-                      (!saw_mid : bool)]
-            done))
-    ;;
-
-    let%expect_test "error cases" =
-      let state = Splittable_random.of_int 0 in
-      let test lo hi =
-        require_does_raise (fun () -> Splittable_random.float state ~lo ~hi)
-      in
-      (* NaN bounds *)
-      test Float.nan 0.;
-      [%expect
-        {|
-        ("float: bounds are not finite numbers"
-          (lo NAN)
-          (hi 0))
-        |}];
-      test 0. Float.nan;
-      [%expect
-        {|
-        ("float: bounds are not finite numbers"
-          (lo 0)
-          (hi NAN))
-        |}];
-      (* infinite bounds *)
-      test Float.neg_infinity 0.;
-      [%expect
-        {|
-        ("float: bounds are not finite numbers"
-          (lo -INF)
-          (hi 0))
-        |}];
-      test 0. Float.infinity;
-      [%expect
-        {|
-        ("float: bounds are not finite numbers"
-          (lo 0)
-          (hi INF))
-        |}];
-      (* crossed bounds *)
-      test 2. 1.;
-      [%expect
-        {|
-        ("float: bounds are crossed"
-          (lo 2)
-          (hi 1))
-        |}]
-    ;;
-  end)
-;;
+  let%expect_test "error cases" =
+    let state = Splittable_random.of_int 0 in
+    let test lo hi =
+      require_does_raise (fun () -> Splittable_random.float state ~lo ~hi)
+    in
+    (* NaN bounds *)
+    test Float.nan 0.;
+    [%expect
+      {|
+      ("float: bounds are not finite numbers"
+        (lo NAN)
+        (hi 0))
+      |}];
+    test 0. Float.nan;
+    [%expect
+      {|
+      ("float: bounds are not finite numbers"
+        (lo 0)
+        (hi NAN))
+      |}];
+    (* infinite bounds *)
+    test Float.neg_infinity 0.;
+    [%expect
+      {|
+      ("float: bounds are not finite numbers"
+        (lo -INF)
+        (hi 0))
+      |}];
+    test 0. Float.infinity;
+    [%expect
+      {|
+      ("float: bounds are not finite numbers"
+        (lo 0)
+        (hi INF))
+      |}];
+    (* crossed bounds *)
+    test 2. 1.;
+    [%expect
+      {|
+      ("float: bounds are crossed"
+        (lo 2)
+        (hi 1))
+      |}]
+  ;;
+end
