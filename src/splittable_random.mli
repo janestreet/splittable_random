c7fbcb1adb35820dcb637cac916f86a0b99cbc58
public-release <as-public-release@janestreet.com>
2025-04-27 21:24:24 +0100

    v0.18~preview.130.26+1192

--- src/splittable_random.mli
+++ src/splittable_random.mli
@@ -1,3 +1,5 @@
+@@ portable
+
 (** A splittable pseudo-random number generator (SPRNG) functions like a PRNG in that it
     can be used as a stream of random values; it can also be "split" to produce a second,
     independent stream of random values.
@@ -6,43 +8,48 @@
     cryptographic-quality randomness in favor of performance.
 
     The primary difference between [Splittable_random] and {!Random} is the [split]
-    operation for generating new pseudo-random states.  While it is easy to simulate
-    [split] using [Random], the result has undesirable statistical properties; the
-    new state does not behave independently of the original.  It is better to switch to
-    [Splittable_random] if you need an operation like [split], as this module has
-    been implemented with the statistical properties of splitting in mind.  For most other
+    operation for generating new pseudo-random states. While it is easy to simulate
+    [split] using [Random], the result has undesirable statistical properties; the new
+    state does not behave independently of the original. It is better to switch to
+    [Splittable_random] if you need an operation like [split], as this module has been
+    implemented with the statistical properties of splitting in mind. For most other
     purposes, [Random] is likely a better choice, as its implementation passes all Diehard
-    tests, while [Splittable_random] fails some Diehard tests.
-*)
+    tests, while [Splittable_random] fails some Diehard tests. *)
 
 open! Base
 
-type t
+type t : value mod portable
 
 (** Create a new [t] seeded from the given random state. This allows nondeterministic
     initialization, for example in the case that the input state was created using
     [Random.make_self_init].
 
     Constructors like [create] and [of_int] should be called once at the start of a
-    randomized computation and the resulting state should be threaded through.
-    Repeatedly creating splittable random states from seeds in the middle of computation
-    can defeat the SPRNG's splittable properties. *)
+    randomized computation and the resulting state should be threaded through. Repeatedly
+    creating splittable random states from seeds in the middle of computation can defeat
+    the SPRNG's splittable properties. *)
 val create : Random.State.t -> t
 
-(** Create a new [t] that will return identical results to any other [t] created with
-    that integer. *)
+(** Create a new [t] that will return identical results to any other [t] created with that
+    integer. *)
 val of_int : int -> t
 
 (** [perturb t salt] adds the entropy of [salt] to [t]. *)
 val perturb : t -> int -> unit
 
 (** Create a copy of [t] that will return the same random samples as [t]. *)
-val copy : t -> t
+val copy : t @ shared -> t
 
-(** [split t] produces a new state that behaves deterministically (i.e. only depending
-    on the state of [t]), but pseudo-independently from [t]. This operation mutates
-    [t], i.e., [t] will return different values than if this hadn't been called. *)
+(** Like [copy], but puts the result in an arbitrary capsule. *)
+val copy_into_capsule : t @ shared -> (t, 'k) Capsule.Data.t
+
+(** [split t] produces a new state that behaves deterministically (i.e. only depending on
+    the state of [t]), but pseudo-independently from [t]. This operation mutates [t],
+    i.e., [t] will return different values than if this hadn't been called. *)
 val split : t -> t
+
+(** Like [split], but puts the result into an arbitrary capsule. *)
+val split_into_capsule : t -> (t, 'k) Capsule.Data.t
 
 (** Legacy aliases for the preceding definitions. *)
 module State : sig
@@ -61,8 +68,8 @@ end
 (** Produces a random, fair boolean. *)
 val bool : t -> bool
 
-(** Produce a random number uniformly distributed in the given inclusive range.  (In the
-    case of [float], [hi] may or may not be attainable, depending on rounding.)  *)
+(** Produce a random number uniformly distributed in the given inclusive range. (In the
+    case of [float], [hi] may or may not be attainable, depending on rounding.) *)
 val int : t -> lo:int -> hi:int -> int
 
 val int32 : t -> lo:int32 -> hi:int32 -> int32
@@ -71,8 +78,8 @@ val int64 : t -> lo:int64 -> hi:int64 -> int64
 val nativeint : t -> lo:nativeint -> hi:nativeint -> nativeint
 val float : t -> lo:float -> hi:float -> float
 
-(** [unit_float state = float state ~lo:0. ~hi:1.], but slightly more efficient (and
-    right endpoint is exclusive). *)
+(** [unit_float state = float state ~lo:0. ~hi:1.], but slightly more efficient (and right
+    endpoint is exclusive). *)
 val unit_float : t -> float
 
 module Log_uniform : sig
